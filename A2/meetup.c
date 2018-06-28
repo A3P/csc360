59886ae560e90af9b8b81484c22b7ec928ce0ec8
Ryan Afshar <ryanafsharryan@gmail.com>
2018-06-27 19:29:21 -0700

    finish a2

--- A2/meetup.c
+++ A2/meetup.c
@@ -12,16 +12,31 @@
  * Declarations for barrier shared variables -- plus concurrency-control
  * variables -- must START here.
  */
-void initialize_meetup(int n, int mf) {
-    char label[100];
-    int i;
+static int group_size;
+static int meet_val;
+static resource_t codeword;
 
+static sem_t sem_single;
+static sem_t sem_group;
+static sem_t sem_leader;
+static int count;
+
+void initialize_meetup(int n, int mf) {
+    
     if (n < 1) {
         fprintf(stderr, "Who are you kidding?\n");
         fprintf(stderr, "A meetup size of %d??\n", n);
         exit(1);
     }
 
+    group_size = n;
+    meet_val = mf;
+    init_resource(&codeword, "meetup");
+
+    sem_init(&sem_single, 0, 1);
+    sem_init(&sem_group, 0, 0);
+    sem_init(&sem_leader, 0, 0);
+    count = 0;
     /*
      * Initialize the shared structures, including those used for
      * synchronization.
@@ -30,5 +45,40 @@ void initialize_meetup(int n, int mf) {
 
 
 void join_meetup(char *value, int len) {
-    printf("NOTHING IMPLEMENTED YET FOR join_meetup\n");
+    int length;
+    if(len>MAX_VALUE_LEN){
+        length=MAX_VALUE_LEN;
+    } else {
+        length = len;
+    }
+    sem_wait(&sem_single);
+    count++;
+    if (count <group_size) {
+        // if meet first, record first value
+        if((meet_val) && (count == 1)){
+            write_resource(&codeword, value, length);
+        }
+        sem_post(&sem_single);
+        sem_wait(&sem_group);
+        read_resource(&codeword, value, MAX_VALUE_LEN);
+        count--;
+        if(count == 1){
+            sem_post(&sem_leader);
+        } else {
+            sem_post(&sem_group);
+        }
+    } else {
+        if(group_size > 1){
+            if(!meet_val) {
+                write_resource(&codeword, value, length);
+            }
+            read_resource(&codeword, value, MAX_VALUE_LEN);
+            sem_post(&sem_group);
+            sem_wait(&sem_leader);
+        } else {
+            write_resource(&codeword, value, length);
+        }
+        count--;
+        sem_post(&sem_single);
+    }
 }
--- A2/rw.c
+++ A2/rw.c
@@ -14,20 +14,67 @@
  */
 
 static resource_t data;
+static int readers;
+static int writers;
+static int active_writers;
+static pthread_mutex_t m;
+static pthread_cond_t readersQ;
+static pthread_cond_t writersQ;
+static char val[MAX_VALUE_LEN];
+
 
 void initialize_readers_writer() {
     /*
      * Initialize the shared structures, including those used for
      * synchronization.
      */
+    init_resource(&data, "rw");
+    readers = 0;
+    writers = 0;
+    active_writers =  0;
+    pthread_mutex_init(&m, NULL);
+    pthread_cond_init(&readersQ, NULL);
+    pthread_cond_init(&writersQ, NULL);
 }
 
 
 void rw_read(char *value, int len) {
-    printf("NOTHING IMPLEMENTED YET FOR rw_read\n");
+    pthread_mutex_lock(&m);
+    while (!(writers == 0)) {
+        pthread_cond_wait(&readersQ, &m);
+    }
+    readers++;
+    pthread_mutex_unlock(&m);
+    read_resource(&data, value, len);
+
+    pthread_mutex_lock(&m);
+    if (--readers == 0) {
+        pthread_cond_signal(&writersQ);
+    }
+    pthread_mutex_unlock(&m);
 }
 
 
 void rw_write(char *value, int len) {
-    printf("NOTHING IMPLEMENTED YET FOR rw_write\n");
+    pthread_mutex_lock(&m);
+    writers++;
+    while(!((readers == 0) && (active_writers == 0))) {
+        pthread_cond_wait(&writersQ, &m);
+    }
+
+    active_writers++;
+    pthread_mutex_unlock(&m);
+    
+    strncpy(val,value,MAX_VALUE_LEN);
+    write_resource(&data,val,len);
+    
+    pthread_mutex_lock(&m);
+    writers--;
+    active_writers--;
+    if (writers) {
+        pthread_cond_signal(&writersQ);
+    } else {
+        pthread_cond_broadcast(&readersQ);
+    }
+    pthread_mutex_unlock(&m);
 }
